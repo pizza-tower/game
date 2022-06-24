7cd7232459c014bdbed618030e4c6e03effcae9c
Carson Long <carsontlong@icloud.com>
2022-06-23 17:41:42 -0700

    wip: pizza peel script

--- Assets/Scripts/PizzaPeelController.cs
+++ Assets/Scripts/PizzaPeelController.cs
@@ -10,7 +10,6 @@ public class PizzaPeelController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         bool KeyDown = Input.GetKeyDown(KeyCode.Space);
