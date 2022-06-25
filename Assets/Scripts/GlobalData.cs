a1f2afd4f11be9715146379ca5f36d3802ca615d
Rajnita Leichombam <36048309+Rajnita@users.noreply.github.com>
2022-06-24 20:33:31 -0700

    Level0
    * Modified Level0 dependencies  in new scripts
    
    * removed unwanted conidtinos
    
    * Level0 rajnita (#17)
    
    * adding horizontal fuse tutorial level changes
    
    * adding fix

--- Assets/Scripts/GlobalData.cs
+++ Assets/Scripts/GlobalData.cs
@@ -8,10 +8,11 @@ public class GlobalData : MonoBehaviour
 {    
 
     public static int verticalFusionHeight=3;
-    public static int level = 0;
-    public static bool isFirstSlice = true;
-    public static string previousSlice="AnchorOne";
-    public static bool isFirstFusionOver = false;
+    public static int level;
+    public static bool isFirstSlice;
+    public static string previousSlice;
+    public static bool isFirstFusionOver;
+    public static bool isFirstHorizontalFusionOver;
     public static bool gameover;
     public static int GoTransparent = 0;
     // Start is called before the first frame update
@@ -21,6 +22,7 @@ public class GlobalData : MonoBehaviour
         isFirstSlice = true;
         previousSlice="AnchorOne";
         isFirstFusionOver = false;
+        isFirstHorizontalFusionOver = false;
         gameover = false;
 
     }
