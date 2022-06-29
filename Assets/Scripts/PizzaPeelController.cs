a0e98445b9f398599ad03c059869f9cfc1f6f527
Carson Long <carsontlong@icloud.com>
2022-06-29 04:39:45 -0700

    record tap as spacebar

--- Assets/Scripts/PizzaPeelController.cs
+++ Assets/Scripts/PizzaPeelController.cs
@@ -15,6 +15,7 @@ public class PizzaPeelController : MonoBehaviour
         bool KeyDown = Input.GetKeyDown(KeyCode.Space);
         bool KeyHold = Input.GetKey(KeyCode.Space);
         bool KeyUp = Input.GetKeyUp(KeyCode.Space); 
+	bool KeyUpMobile = Input.GetKeyUp(KeyCode.Tap);
         //control the flipper with space bar
         if(KeyDown && KeyHold && !KeyUp)
         {
