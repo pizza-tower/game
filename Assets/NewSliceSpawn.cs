4a0a1377d5a0c36542c79675a05f549c2b46661d
natsoleary <61570958+natsoleary@users.noreply.github.com>
2022-07-19 23:25:43 -0400

    Revert "Merge branch 'main' into UI-Stars-Natalie"
    This reverts commit acbf496ccebab35a6ccfb68c74e5af42abffb706, reversing
    changes made to 2c150b936ca32cb4e04655b55f6c42bbe1bc57c3.

--- Assets/NewSliceSpawn.cs
+++ Assets/NewSliceSpawn.cs
@@ -10,6 +10,8 @@ public class NewSliceSpawn : MonoBehaviour
 {
     public GameObject Slice;
     public GameObject endlevelscreen;
+    public GameObject winscreen;
+    public GameObject gameoverscreen;
     float NewSliceSpawnSeconds;
     public int NeedsNewSlice = 1;
     public int NumberSpawned = 0;
@@ -30,14 +32,15 @@ public class NewSliceSpawn : MonoBehaviour
             {
                 NeedsNewSlice = 0;
                 // TODO: Trigger game over
-                if (instantiated == false) {
-                    GameObject popup = Instantiate(endlevelscreen);
-                    instantiated = true;
-                    Transform finalscore = popup.transform.GetChild(1);
-                    TextMeshProUGUI scoretext = finalscore.gameObject.GetComponent<TextMeshProUGUI>();
-                    GameObject plate = GameObject.FindWithTag("Plate");
-                    Score score =  plate.GetComponent<Score>();
-                    scoretext.SetText("score: {0}", score.GetScoreSummary().starsEarned);
+
+                int level = SceneManager.GetActiveScene().buildIndex;
+                if (instantiated == false && level + 1 < GlobalData.totalscenes ) {
+                    StartCoroutine(PopUp(gameoverscreen));
+         
+                }
+                
+                else if (instantiated == false) {
+                    StartCoroutine(PopUp(winscreen));
                 }
                 return;
             }
@@ -50,6 +53,45 @@ public class NewSliceSpawn : MonoBehaviour
         yield return new WaitForSeconds(NewSliceSpawnSeconds);
         NewSliceSpawnSeconds = 1.3f;
         spawnSlice();
+    }
+    IEnumerator PopUp(GameObject panel) {
+        yield return new WaitForSeconds(2);
+                    GameObject popup = Instantiate(panel);
+                    instantiated = true;
+                    GameObject star1 = popup.transform.GetChild(1).gameObject;
+                    GameObject star2 = popup.transform.GetChild(2).gameObject;
+                    GameObject star3 = popup.transform.GetChild(3).gameObject;
+                    GameObject plate = GameObject.FindWithTag("Plate");
+                    Score score =  plate.GetComponent<Score>();
+                    if (score.GetScoreSummary().starsEarned >= 1) {
+                        star1.SetActive(true);
+                    }
+                    if (score.GetScoreSummary().starsEarned >= 2) {
+                        star2.SetActive(true);
+                    }
+                    if (score.GetScoreSummary().starsEarned >= 3) {
+                        star3.SetActive(true);
+                    }
+                    Transform vertscore = popup.transform.GetChild(4);
+                    TextMeshProUGUI verttext = vertscore.gameObject.GetComponent<TextMeshProUGUI>();
+                    verttext.SetText("# Vertical Fusions = {} x 5 = {} points", score.GetScoreSummary().numVerticalFusions, score.GetScoreSummary().scoreVerticalFusions);    
+
+                    Transform horscore = popup.transform.GetChild(5);
+                    TextMeshProUGUI hortext = horscore.gameObject.GetComponent<TextMeshProUGUI>();
+                    hortext.SetText("# Horizontal Fusions = {} x 20 = {} points", score.GetScoreSummary().numHorizontalFusions, score.GetScoreSummary().scoreHorizontalFusions);    
+
+                    Transform powscore = popup.transform.GetChild(6);
+                    TextMeshProUGUI powtext = powscore.gameObject.GetComponent<TextMeshProUGUI>();
+                    powtext.SetText("# Powers Used = {} x 5 = {} points", score.GetScoreSummary().numPowersUsed, score.GetScoreSummary().scorePowersUsed);    
+
+                    Transform slicescore = popup.transform.GetChild(7);
+                    TextMeshProUGUI slicetext = slicescore.gameObject.GetComponent<TextMeshProUGUI>();
+                    slicetext.SetText("# Slices Left = {} x 5 = {} points", score.GetScoreSummary().numSlicesLeft, score.GetScoreSummary().scoreSlicesLeft);    
+
+                    Transform totalscore = popup.transform.GetChild(8);
+                    TextMeshProUGUI totaltext = totalscore.gameObject.GetComponent<TextMeshProUGUI>();
+                    totaltext.SetText("Total Score = {} points", score.GetScoreSummary().scoreTotal);    
+
     }
     public void spawnSlice() 
     {
