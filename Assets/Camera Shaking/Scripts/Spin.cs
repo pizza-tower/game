622fceeab092932fcba6207dc43aad1160c28d7b
Thomas Ning <zhiyuann@usc.edu>
2022-07-21 12:53:46 -0700

    Camera shaking2 (#84)
    * Camera Shaking added
    
    * add the Camera shaking level 1

---
+++ Assets/Camera Shaking/Scripts/Spin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.Rotate(0f, 0f, speed);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            CameraShaking.Action.Stop();
+        }
+    }
+}
